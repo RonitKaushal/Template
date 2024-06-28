1a53b911395739278513970ace8ef14f822fd650
System Administrator <root@Ronits-MacBook.local>
2024-06-28 18:22:36 +0530

    made some changes by RK

--- backend/frontend/src/App.jsx
+++ backend/frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { LoginForm } from "@/components/demo/LoginForm"
+import { RegisForm } from "@/components/demo/RegisForm"
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "@/components/mode-toggle"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BreadcrumbDemo } from "./components/demo/BreadcrumbDemo"
+import { Home } from "./components/demo/Home"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Code by Ronit for TESTING!!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+      <BreadcrumbDemo/>
+        <Routes>
+          <Route path="login" element={<LoginForm></LoginForm>} />
+          <Route path="register" element={<RegisForm></RegisForm>} />
+          <Route path="/home" element={<Home></Home>} />
+        </Routes>
+      </BrowserRouter>
+    <ModeToggle></ModeToggle>
+    </ThemeProvider>
   )
 }
-
-export default App
+export default App;
--- backend/utils/db.js
+++ backend/utils/db.js
@@ -6,7 +6,7 @@ const DB = async () =>{
         await mongoose.connect(process.env.MONGO_URL)
         console.log('DB connection success')
     } catch (error) {
-        console.log(error)      
+        console.log("MONGO URI Error: ", error)      
     }
 }
 
