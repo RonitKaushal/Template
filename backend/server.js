8749a18ceacea11a4981cd4f034b62357c9adfce
Tirth chokshi <chokshitirth4@gmail.com>
2024-06-28 13:57:03 +0530

    frontend+backend done

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,12 @@ import DB from './utils/db.js'
 import router from './routes/auth.js'
 import cookieParser from 'cookie-parser'
 import adminRoutes from './routes/adminRoute.js'
+import { fileURLToPath } from 'url'
 
 
 dotenv.config()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const PORT = process.env.PORT
 const app = express()
 
@@ -21,8 +24,8 @@ app.use('/api/auth',router)
 app.use('/api/admin',adminRoutes)
 
 app.get('/',(req,res) =>{
-    app.use(express.static(path.resolve(__dirname,"frontend","build")))
-    res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
+    app.use(express.static(path.resolve(__dirname,"frontend","dist")))
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
   })
 app.listen(PORT, () => {
     DB()
