7e9985e1d1c05af39493e03baae0e53215dbf99b
System Administrator <root@Ronits-MacBook.local>
2024-06-28 20:30:03 +0530

    Login functionality added

--- backend/frontend/src/components/elements/LogCompo.jsx
+++ backend/frontend/src/components/elements/LogCompo.jsx
@@ -1,3 +1,4 @@
+import * as React from "react"
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -15,8 +16,82 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
+import { useNavigate } from "react-router-dom";
+
+import { Toaster, toast } from 'sonner'
 
 export function LogCompo() {
+
+
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+      })
+
+      const navigator = useNavigate();
+    
+      const handleInput = (e) =>
+        {
+          let name = e.target.name;
+          let value = e.target.value;
+        
+          setUser({
+            ...user,
+            [name]: value,
+          });
+    
+        };
+    
+        const handleLoginSubmit = async (e) =>
+          {
+            e.preventDefault();
+            console.log("Clicked...!");
+
+            try{
+            const response = await fetch(`localhost:5173/api/auth/login`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(user),
+            });
+            console.log(response);
+            if(response.ok)
+              {
+                setUser({    
+                  email: "",
+                  password: "",});
+
+                  navigator("/home")
+              }
+              else
+              {
+                console.log("response failed...!");
+
+                toast("Register Failed...!", {
+                  description: "Please use correct credencials",
+                  action: {
+                    label: "Continue",
+                    onClick: () => console.log("Undo"),
+                  },
+                })
+              }
+          }
+          catch (error)
+          {
+            console.log("Register error: ", error);
+
+            toast("Server Error", {
+                description: "There is something worng with the server",
+                action: {
+                  label: "Continue",
+                  onClick: () => console.log("Undo"),
+                },
+              })
+
+          }
+          };
+
   return (
     <div className="LogCompo_center">
     <Tabs defaultValue="account" className="w-[400px]">
@@ -25,6 +100,7 @@ export function LogCompo() {
         <TabsTrigger value="password">Register</TabsTrigger>
       </TabsList>
       <TabsContent value="account">
+      <form onSubmit={handleLoginSubmit}>
         <Card>
           <CardHeader>
             <CardTitle>Login</CardTitle>
@@ -35,17 +111,18 @@ export function LogCompo() {
           <CardContent className="space-y-2">
             <div className="space-y-1">
               <Label htmlFor="email">Email</Label>
-              <Input id="email" placeholder="Please enter your email" />
+              <Input id="email" placeholder="Please enter your email" name="email" value={user.email} onChange={handleInput} />
             </div>
             <div className="space-y-1">
               <Label htmlFor="password">Password</Label>
-              <Input id="password" placeholder="Please enter your password" />
+              <Input id="password" placeholder="Please enter your password" name="password" value={user.password} onChange={handleInput} />
             </div>
           </CardContent>
           <CardFooter>
             <Button>Continue</Button>
           </CardFooter>
         </Card>
+        </form>
       </TabsContent>
       <TabsContent value="password">
         <Card>
@@ -75,6 +152,7 @@ export function LogCompo() {
         </Card>
       </TabsContent>
     </Tabs>
+    <Toaster />
     </div>
   )
 }
